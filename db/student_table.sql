d0f8d955a1a1458c30add68f88c38afe473e15d3
Achal Chaudhari <141348915+Achal0356@users.noreply.github.com>
2025-08-04 00:06:24 +0530

    Create student_table.sql

---
+++ db/student_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE students (
+  student_id INT PRIMARY KEY AUTO_INCREMENT,
+  student_name VARCHAR(100),
+  student_addr VARCHAR(255),
+  student_age INT,
+  student_qual VARCHAR(100),
+  student_percent DECIMAL(5,2),
+  student_year_passed INT
+);
